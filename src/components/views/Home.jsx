6d4e6ebd4b06ad7b43cbbd4dcd53e0ebf0426481
Michael Galo <michael@michaelthinks.co>
2024-09-25 07:24:57 -0500

    Updated Contact

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -106,7 +106,7 @@ export const Home = () => {
             <section id="contact" className="mb-8 max-w-4xl mx-auto">
                 <h2 className="text-2xl font-semibold mb-2">Contact</h2>
                 <div className="bg-gray-200 dark:bg-gray-800 p-4 rounded-lg shadow-md">
-                    <p>If you'd like to get in touch, feel free to reach out via email at <a target="_blank" href="mailto:michael@michaelthinks.co" className="text-blue-500 dark:text-blue-300">michael@michaelthinks.co</a>.</p>
+                    <p>If you'd like to get in touch, feel free to reach out via email at <a target="_blank" href="mailto:michael@michaelthinks.co" className="text-blue-500 dark:text-blue-300">michael@michaelthinks.co</a> or find me <a target="_blank" href="https://linktr.ee/michaelthinks" className="text-blue-500 dark:text-blue-300">HERE</a> on social media.</p>
                 </div>
             </section>
         </div>
