5754938e790259943eaedaf19679795139ab107d
Michael Galo <michael@michaelthinks.co>
2024-10-31 09:55:32 -0500

    updated tech scroller

--- src/components/ScrollingLogoSlider.jsx
+++ src/components/ScrollingLogoSlider.jsx
@@ -1,5 +1,5 @@
 const logos = [
-  "/assets/HTML.png", "/assets/CSS.png", "/assets/JS.png", "/assets/React.png", "/assets/Python.png", "/assets/Django.png", "/assets/Git.png", "/assets/GitHub.png", "/assets/Tailwind.png", "/assets/Vscode.png", "/assets/Postman.png" , "/assets/Bootstrap.png"
+  "/assets/HTML.png", "/assets/CSS.png", "/assets/JS.png","assets/TS.png","assets/Next.png", "/assets/React.png", "/assets/Python.png", "/assets/Django.png", "/assets/Git.png", "/assets/GitHub.png", "/assets/Tailwind.png", "/assets/Vscode.png", "/assets/Postman.png" , "/assets/Bootstrap.png"
 ];
 
 export const ScrollingLogoSlider = () => {
