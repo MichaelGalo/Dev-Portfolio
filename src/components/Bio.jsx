1c7ce379398ac8f7bd5ab600a2a940d69a05bedd
Michael Galo <michael@michaelthinks.co>
2024-12-02 20:41:39 -0600

    Updated introduction

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,22 +1,38 @@
 export const Bio = () => {
-    return (
-<>
-    <section id="bio" className="mb-8 max-w-4xl mx-auto">
+  return (
+    <>
+      <section id="bio" className="mb-8 max-w-4xl mx-auto">
         <h2 className="text-2xl font-semibold mb-2">Bio</h2>
         <p className="bg-gray-200 dark:bg-gray-800 p-4 rounded-lg shadow-md">
-            Hey there, I'm Michael, I'm a junior full-stack web developer entering the tech industry from the side-door.
-            <blockquote className="flex items-center justify-center my-6">
-                    <p className="text-center italic text-lg">
-                        "If you don't like the road you're walking, start paving another one!"
-                        <footer className="mt-2 text-sm not-italic">- Dolly Parton</footer>
-                    </p>
-            </blockquote> 
-
-            Transitioning from a first career in marketing & communications primarily for non-profits & B-Corp brands, I hadn't quite found the analytical challenge I was craving in my career. Combine this with a lego-filled childhood and the university path never walked named <strong>computer science</strong>, I decided to take Dolly's advice and pave my own path into: <strong>software development</strong>. 
-            <br />
-            <br />
-            After learning to solve some real-life headaches with code, I discovered an intoxicating recipe of logic, creativity, humbling debugging and deep fulfillment in the projects I was working on. After that, I knew I wasn't going back. This is the most fun I've had with my work in years and my brain was beings stretched more than ever. I can't wait to see how many more surprises software development has in store for me and how I can continue to turn novel ideas into apps that make an impact, solve problems, and make people smile. 
+          Hey there, I'm Michael, I'm a full-stack web developer entering the
+          tech industry from the side-door.
+          <blockquote className="flex items-center justify-center my-6">
+            <p className="text-center italic text-lg">
+              "If you don't like the road you're walking, start paving another
+              one!"
+              <footer className="mt-2 text-sm not-italic">
+                - Dolly Parton
+              </footer>
+            </p>
+          </blockquote>
+          Transitioning from a first career in marketing & communications
+          primarily for non-profits & B-Corp brands, I hadn't quite found the
+          analytical challenge I was craving in my career. Combine this with a
+          lego-filled childhood and the university path never walked named{" "}
+          <strong>computer science</strong>, I decided to take Dolly's advice
+          and pave my own path into: <strong>software development</strong>.
+          <br />
+          <br />
+          After learning to solve some real-life headaches with code, I
+          discovered an intoxicating recipe of logic, creativity, humbling
+          debugging and deep fulfillment in the projects I was working on. After
+          that, I knew I wasn't going back. This is the most fun I've had with
+          my work in years and my brain was beings stretched more than ever. I
+          can't wait to see how many more surprises software development has in
+          store for me and how I can continue to turn novel ideas into apps that
+          make an impact, solve problems, and make people smile.
         </p>
-    </section>
-</>)
-}
+      </section>
+    </>
+  );
+};
